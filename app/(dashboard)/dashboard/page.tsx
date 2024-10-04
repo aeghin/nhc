96eb0c298e13974c3ef8a23e394a01f75b678ba0
Edward Castaneda <aeghin7@gmail.com>
2024-10-04 13:20:05 -0500

    testing tanstack query

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,25 @@
+'use client'
+
+import { useQuery } from "@tanstack/react-query"
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal, AwaitedReactNode } from "react";
+
+
 export default function Dashboard() {
+
+    const getPokemonInfo = async () => {
+        const url = 'https://66e740d117055714e58bd904.mockapi.io/api/v1/users';
+        const response = await fetch(url);
+        // console.log(response);
+        return response.json();
+    };
+
+    const { data: users } = useQuery({ queryKey: ['pokemonData'], queryFn: getPokemonInfo })
+    console.log(users);
+
+
     return (
-        <div>dashboard page (protected route)</div>
+        <div>{users?.map((user: any) => (
+            <h1 className="flex justify-center" key={user.id}>{user.name}</h1>
+        ))}</div>
     )
 };
