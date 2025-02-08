68f1f734c3de5c3ec3496ffc67a45acc68f12164
Edward Castaneda <88182227+aeghin@users.noreply.github.com>
2025-02-07 22:07:34 -0600

    Update route.ts

--- app/api/getSession/route.ts
+++ app/api/getSession/route.ts
@@ -4,6 +4,10 @@ import { NextResponse } from "next/server";
 export async function GET() {
     const { sessionId } = await auth();
 
+    if (!sessionId) {
+     return NextResponse.json("session not found", {status: 401 });
+};
+
     return NextResponse.json(JSON.stringify({ sessionId }), { status: 200 });
     
 };
