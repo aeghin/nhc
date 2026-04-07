80db2416d914ecb42072ec049d0f0d6743dd4254
Edward Castaneda <aeghin7@gmail.com>
2026-04-06 20:21:03 -0500

    fix: proxy.ts changes to prevent edge requests

--- proxy.ts
+++ proxy.ts
@@ -1,30 +1,16 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
-  "/api/(.*)",
   "/setup(.*)",
 ]);
 
-const isWebhookRoute = createRouteMatcher(["/api/webhooks(.*)"]);
-
 export default clerkMiddleware(async (auth, req) => {
-  if (isWebhookRoute(req)) return;
-
   if (isProtectedRoute(req)) {
     await auth.protect();
-  }
-
-  const { userId } = await auth();
-
-  if (!userId) return;
-
-  if (req.nextUrl.pathname === "/") {
-    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
 });
 
 export const config = {
-  matcher: ["/", "/dashboard(.*)", "/setup(.*)", "/api(.*)", "/invite(.*)"],
+  matcher: ["/dashboard(.*)", "/setup(.*)"],
 };
